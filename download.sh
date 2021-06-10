120e8b2a27eeedcc88c12dbf9f2ea547f4b49688
Andy WuLu <andywulu914@gmail.com>
2021-06-10 12:38:07 +0800

    Add two bash scripts into the repo

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget https://www.dropbox.com/s/d9oxng2td42nzs5/checkpoint-10856.zip?dl=1 -O ./checkpoint-10856.zip
+unzip ./checkpoint-10856.zip
---
+++ run.sh
@@ -0,0 +1,1 @@
+python3.8 test.py --input_file "${1}" --output_file "${2}"
